aa4424862cf65269f47d15155dc31b87231b1a90
bevhatzl <beverleyhatzl@gmail.com>
2023-03-13 21:38:47 +1100

    create the game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen() {
+function StartGameScreen({ onConfirmNumber }) {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   function numberInputHandler(enteredText) {
@@ -24,7 +24,7 @@ function StartGameScreen() {
       );
       return;
     }
-    console.log('valid number');
+    onConfirmNumber(chosenNumber);
   }
 
   return (
